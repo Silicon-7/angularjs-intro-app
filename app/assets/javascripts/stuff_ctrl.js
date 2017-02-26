5182b463cd63708a041a23b20136235402b4fee5
JackMarx <seriousfools@gmail.com>
2017-02-26 11:35:16 -0600

    complex models

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -3,13 +3,38 @@
 
   angular.module("app").controller("stuffCtrl", function($scope){
     $scope.reviews = [
-                      "Angular is awesome", 
-                      "Cant't beat the Ng", 
-                      "I like React better"
+                        {
+                          text: "Angular is awesome",
+                          rating: 5,
+                          reviewer: "Mark"
+                        }, 
+                        {
+                          text: "Cant't beat the Ng",
+                          rating: 4,
+                          reviewer: "Susan"
+                        }, 
+                        {
+                          text: "I like React better",
+                          rating: 1,
+                          reviewer: "Patricia"
+                        }
                       ];
 
-    $scope.addReview = function(review){
-      $scope.reviews.push(review);
+    $scope.addReview = function(reviewText, reviewRating, reviewReviewer){
+      var reviewHash = {
+                        text: reviewText,
+                        rating: reviewRating,
+                        reviewer: reviewReviewer
+                        };
+      $scope.reviews.push(reviewHash);
+      $scope.newReviewText = null;
+      $scope.newReviewRating = null;
+      $scope.newReviewReviewer = null;
+    };
+
+    $scope.isPositive = function(review) {
+      return review.indexOf("stupid") === -1;
+      true
     };
 
     window.scope = $scope;
